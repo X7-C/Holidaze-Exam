d7be2b85667c7bab670afc83fcff5207ae60a2f7
Max <ussrspartanx@gmail.com>
2025-05-25 21:01:09 +0200

    Update venueService.ts

--- services/venueService.ts
+++ services/venueService.ts
@@ -17,15 +17,21 @@ export const getVenueById = async (id: string, params?: Record<string, any>) => 
 
 
 export const createVenue = async (data: any) => {
-  return apiRequest(
-    '/holidaze/venues',
-    {
-      method: 'POST',
-      body: JSON.stringify(data),
+  const response = await apiRequest('/holidaze/venues', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
     },
-    true
-  );
+  }, true);
+
+  if (response.error) {
+    throw new Error(response.error);
+  }
+
+  return response.data;
 };
+
 
 export const updateVenue = async (id: string, venueData: any) => {
   return apiRequest(
